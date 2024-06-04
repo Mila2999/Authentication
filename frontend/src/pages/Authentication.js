a252469319263b5bce7e74f23269f552f7f89114
mila2999 <mila.savonik@gmail.com>
2024-06-04 21:01:05 +0300

    Working on authActions

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { json, redirect } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -6,4 +7,35 @@ function AuthenticationPage() {
 
 export default AuthenticationPage;
 
-// /auth
+export async function action({ request }) {
+  console.log('action working', request);
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
+  console.log(mode);
+  if (mode !== 'login' && mode !== 'signup') {
+    console.log('if');
+    throw json({ message: 'Unsupported mode.' }, { status: 422 });
+  }
+
+  const data = request.formData();
+  console.log(data);
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  };
+  console.log('authData: ' + authData);
+  const response = await fetch('http://localhost:8080/' + mode, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json ' },
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+  if (!response.ok) {
+    throw json({ message: 'Could not autentificate user' }, { status: 500 });
+  }
+  // soon: manage that token
+  return redirect('/');
+}
